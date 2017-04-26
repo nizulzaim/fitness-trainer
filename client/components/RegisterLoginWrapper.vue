c90b744dd28b5f755a04f58dad46f4369bf134e9
Nizul Zaim <mnizulzaim@gmail.com>
2017-04-26 08:35:36 +0800

    change image background

--- client/components/RegisterLoginWrapper.vue
+++ client/components/RegisterLoginWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="background-deep-purple" v-wheight="100" >
+    <div class="background-theme" v-wheight="100" >
         <div class="__background-wrap">
             <div class="__background"></div>
         </div>
@@ -21,7 +21,7 @@
         height: 100%;
         -webkit-backface-visibility: hidden;
         .__background {
-            background: url(/img/b.jpg) center;
+            background: url(/img/back-min.jpg) center;
             background-size: cover;
             filter: blur(3px);
             position: absolute;
---
+++ mobile-config.js
@@ -0,0 +1,7 @@
+App.info({
+   id: 'com.devsign.my.fitrainer',
+   name: 'Fitrainer',
+   version: "0.0.1"
+});
+
+App.setPreference('BackgroundColor', '0xff0000ff');
